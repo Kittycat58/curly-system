a537a23ff1175f5f976ec5ceef7859a005f04cc5
Kittycat58 <127146651+Kittycat58@users.noreply.github.com>
2023-10-21 12:40:46 +0530

    Add files via upload

---
+++ Intermediate Level/Quiz Project/data.py
@@ -0,0 +1,12 @@
+question_data = [
+    {"question":"The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.","correct_answer":"False","incorrect_answers":["True"]},
+    {"question":"The logo for Snapchat is a Bell.","correct_answer":"False","incorrect_answers":["True"]},
+    {"question":"Pointers were not used in the original C programming language; they were added later on in C++.","correct_answer":"False","incorrect_answers":["True"]},
+    {"question":"RAM stands for Random Access Memory.","correct_answer":"True","incorrect_answers":["False"]},
+    {"question":"Ada Lovelace is often considered the first computer programmer.","correct_answer":"True","incorrect_answers":["False"]},
+    {"question":"&quot;HTML&quot; stands for Hypertext Markup Language.","correct_answer":"True","incorrect_answers":["False"]},
+    {"question":"In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.","correct_answer":"True","incorrect_answers":["False"]},
+    {"question":"The Windows 7 operating system has six main editions.","correct_answer":"True","incorrect_answers":["False"]},
+    {"question":"The Windows ME operating system was released in the year 2000.","correct_answer":"True","incorrect_answers":["False"]},
+    {"question":"Linux was first created as an alternative to Windows XP.","correct_answer":"False","incorrect_answers":["True"]}
+]
